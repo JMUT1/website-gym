9339ceacd391420812d5860484fe3188160c5970
Chema Torres <chemaaa94@gmail.com>
2022-12-29 11:20:55 -0600

    adding the pages to the APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Gallery from "./pages/gallery/Gallery";
+import NotFound from "./pages/notFound/NotFound";
+import Plans from "./pages/plans/Plans";
+import Trainners from "./pages/trainners/Trainners";
 
 function App() {
-  return <div>Gym</div>;
+  return (
+    <div>
+      <Home />
+      <About />
+      <Contact />
+      <Gallery />
+      <NotFound />
+      <Plans />
+      <Trainners />
+    </div>
+  );
 }
 
 export default App;
