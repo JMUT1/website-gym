21d4df92b70be16665f542bdc23d5f54d03c9f9b
Chema Torres <chemaaa94@gmail.com>
2022-12-29 16:56:14 -0600

    create new components that bind information

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import MainHeader from "../../components/MainHeader";
+import Programs from "../../components/Programs";
 
 import "./home.css";
 
@@ -6,6 +7,7 @@ function Home() {
   return (
     <>
       <MainHeader />
+      <Programs />
     </>
   );
 }
