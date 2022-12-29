c9930505e2f6d92f403a72de88a447d7d327dae9
Chema Torres <chemaaa94@gmail.com>
2022-12-29 17:06:13 -0600

    create a CAR and use it in the programs component so I map the info in de datsjs

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { FaCrown } from "react-icons/fa";
 import SectionHead from "./SectionHead";
+import { programs } from "../data";
+import { Card } from "../IU/Card";
 
 const Programs = () => {
   return (
     <section className="programs">
       <div className="container programs__container">
         <SectionHead icon={<FaCrown />} title="Programs" />
+      </div>
+
+      <div className="programs__wrapper">
+        {programs.map(({ id, icon, title, info, path }) => {
+          return (
+            <Card className="programs__program" key={id}>
+              <span>{icon}</span>
+              <h4>{title}</h4>
+              <small>{info}</small>
+            </Card>
+          );
+        })}
       </div>
     </section>
   );
