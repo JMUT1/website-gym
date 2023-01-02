b88a2bf54ffef626cf1569a7ac22ddefab3aade1
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-02 17:18:53 -0600

    making the plans jsx

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+
+import {plans} from '../../data'
+import Head from "../../components/Head"
+import Card from "../../IU/Card";
+import HeaderImg from "../../images/header_bg_4.jpg"
 import "./plans.css";
 
 export default function Plans() {
-  return <div>Plans</div>;
+  return <>
+  <Head title="Membership Plans" image={HeaderImg}>
+    Descubre los planes que tenemos para ti
+  </Head>
+  <section className='plans'>
+    <div className='container plans__container'>
+      {
+        plans.map(({id,name, desc, price, features})=>{
+          return <Card key={id} className="plan">
+            <h3>{name}</h3>
+            <small>{desc}</small>
+            <h1>{`$${price}`}</h1><h2>/mo</h2>
+            <h4>Features</h4>
+            {
+              features.map(({feature,available},index)=>{
+                return <p key={index} className={!available ? 'disable' : ""}>{feature}</p>
+              })
+            }
+            <button className='btn lg'>Choose Plan</button>
+          </Card>
+        })
+      }
+    </div>
+  </section>
+
+  
+  
+  </>
 }
