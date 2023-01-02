0d8d9721650846461cd4441121597000e8576f06
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-02 17:02:11 -0600

    easy notfound page

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import {Link} from "react-router-dom"
+
 import "./notfound.css";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return <section>
+    <div className="container notfound__container">
+      <h2>Page not Found</h2>
+      <Link to="/" className="btn">Go back Home</Link>
+    </div>
+  </section>;
 }
 
 export default NotFound;
