ac8831771a39b5c7b2c7b6918834b99bc9a6e26a
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-02 12:55:56 -0600

    creating the contact page and the media querys

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import Head from "../../components/Head"
+import HeaderImg from "../../images/header_bg_2.jpg"
+import {MdEmail} from 'react-icons/md'
+import {BsMessenger} from "react-icons/bs"
+import {IoLogoWhatsapp} from "react-icons/io"
+
+
 import "./contact.css";
 
 function Contact() {
-  return <div>Contact</div>;
+  return <>
+  <Head title="Get in Touch" image={HeaderImg}>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi aut ipsam eligendi eveniet, debitis adipisci.
+  </Head>
+  <section className="contact">
+    <div className="container contact__container">
+      <div className="contact__wrapper">
+        <a href="mailto:gmail@gmail.com" target="_blank" rel="noreferrer noopener"><MdEmail/></a>
+        <a href="http://m.me/chemaT" target="_blank" rel="noreferrer noopener"><BsMessenger/></a>
+        <a href="https://wa.me/+528112550227" target="_blank" rel="noreferrer noopener"><IoLogoWhatsapp/></a>
+
+      </div>
+    </div>
+  </section>
+  
+  
+  </>;
 }
 
 export default Contact;
